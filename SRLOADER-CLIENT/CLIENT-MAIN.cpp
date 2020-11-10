b1c0d374ee024014c9c47db9f16246cabf181c56
numero69 <numerouno693@gmail.com>
2020-11-10 11:35:46 +0100

    Add files via upload

---
+++ SRLOADER-CLIENT/CLIENT-MAIN.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//i recommend installing cpr with vcpkg
+#include <cpr/cpr.h>
+//make sure you do not forget to add the include directory of your json.hpp
+#include <json.hpp>
+int main()
+{
+    std::cout << "Hello World!\n";
+}
